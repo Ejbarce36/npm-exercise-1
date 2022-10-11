836550277a4f486b52e9aaea8f5171a882e568af
Emmanuelle Jim Arce <emmanuelle.arce@emmanuelle.arce-CKQ1H46KXL>
2022-10-11 09:38:17 +0800

    added file .prettierrc and use prettier

---
+++ index.js
@@ -0,0 +1,5 @@
+const qrcode = require('qrcode-terminal');
+
+qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;H:<true>;;');
+
+qrcode.generate('BEGIN:VCARD\nVERSION:3.0\nTEL;TYPE=work,VOICE:(0917) 397-4095\nEMAIL:ejbarce36@gmail.com\nN:EJ Arce\nEND:VCARD');
